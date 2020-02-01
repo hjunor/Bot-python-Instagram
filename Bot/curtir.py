04b07e207e0834a790805e3eed24522d316746fd
hjunor <hjunor@gmail.com>
2020-02-01 10:44:38 -0300

    Configuração de curtir e comentar

---
+++ Bot/curtir.py
@@ -0,0 +1,16 @@
+from instapy import InstaPy
+from instapy import smart_run
+
+session = InstaPy(username='up4.0' , password='3n5df3k9')
+
+with  smart_run(session):
+    session.set_do_follow(enabled=True, percentage=100)
+    session.set_do_like(enabled=True, percentage=100)
+
+    session.like_by_tags(['tech','javascript'])
+
+    comentarios = ['Very good', 'Nice short']
+
+    session.set_do_comment(enabled=True, percentage=95)
+    session.set_comments(comentarios,media='Photo')
+    session.join_pods()
