83e0183f7f482cf8f970e9c4c0593eb8c959a407
hjunor <hjunor@gmail.com>
2020-02-02 02:58:20 -0800

    especificações

--- Bot/curtir.py
+++ Bot/curtir.py
@@ -4,12 +4,12 @@ from instapy import smart_run
 session = InstaPy(username='up4.0' , password='3n5df3k9')
 
 with  smart_run(session):
-    session.set_do_follow(enabled=True, percentage=100)
+    session.set_do_follow(enabled=True, percentage=100) 
     session.set_do_like(enabled=True, percentage=100)
 
-    session.like_by_tags(['tech','javascript'])
+    session.like_by_tags(['tech','javascript']) """ Tags para o bot seguir """
 
-    comentarios = ['Very good', 'Nice short']
+    comentarios = ['Very good', 'Nice short'] """Comentarios pré definidos """
 
     session.set_do_comment(enabled=True, percentage=95)
     session.set_comments(comentarios,media='Photo')
